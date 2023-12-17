1c0ea998bf881ee541cbae2d40f64887edf67863
Lee Wonju <dldnjswn4265@gmail.com>
2023-12-17 23:36:30 +0900

    feat #1 : 라우터  버전업 및 HomePage 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,34 @@
-import { Route, Routes } from 'react-router-dom';
-import GuestBookPage from './pages/guestbook/GuestBookPage';
-import FootprintPage from './pages/footprint/FootprintPage';
-import LoginPage from './pages/login/LoginPage';
-import SignUpPage from './pages/signup/SignUpPage';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import GuestBookPage from "./pages/guestbook/GuestBookPage";
+import FootprintPage from "./pages/footprint/FootprintPage";
+import LoginPage from "./pages/login/LoginPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import HomePage from "./pages/home/HomePage";
 
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage />,
+  },
+  {
+    path: "/guestbook",
+    element: <GuestBookPage />,
+  },
+  {
+    path: "/footprint",
+    element: <FootprintPage />,
+  },
+  {
+    path: "/login",
+    element: <LoginPage />,
+  },
+  {
+    path: "/signup",
+    element: <SignUpPage />,
+  },
+]);
 function App() {
-  return (
-    <div>
-      <Routes>
-        <Route path="/guestbook" element={<GuestBookPage />} />
-        <Route path="/footprint" element={<FootprintPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-      </Routes>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
---
+++ src/pages/home/HomePage.tsx
@@ -0,0 +1,9 @@
+function HomePage() {
+  return (
+    <div className="text-center">
+      <p className="text-3xl font-bold">홈화면</p>
+    </div>
+  );
+}
+
+export default HomePage;
