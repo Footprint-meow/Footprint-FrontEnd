e50afe4f27276d0a88cd57174c0cf50d49a49b4c
Lee Wonju <dldnjswn4265@gmail.com>
2023-12-24 12:48:43 +0900

    feat #9 : 뒤로가기 구현

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,16 @@
 import React from "react";
 import { MenuTypeProp } from "../types/header";
+import { useNavigate } from "react-router-dom";
 const Header = (menu: MenuTypeProp) => {
   const {left, right} = menu.menu;
-
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  }
+  
   return (
     <div className="w-full flex justify-between px-5">
-      <div>{left}</div>
+      <div onClick={goBack}>{left}</div>
       <div>{right}</div>
     </div>
   );
