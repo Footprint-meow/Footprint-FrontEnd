2e199b45658da72aeb35f30dde25166323c9cc4c
hyun812 <sh980812@naver.com>
2023-12-25 01:30:39 +0900

    style #11 : 사진첩 보기 UI 구현

--- src/pages/footprint/components/PhotoAlbumTab.tsx
+++ src/pages/footprint/components/PhotoAlbumTab.tsx
@@ -1,5 +1,19 @@
 function PhotoAlbumTab() {
-  return <div>사진첩 보기</div>;
+  return (
+    <div className="w-full">
+      {/* 날자별 반복 */}
+      <div className="pt-6">
+        <p className="px-4 font-bold">2023년 12월 14일</p>
+        {/* 발자국별 반복 */}
+        <div className="grid grid-cols-3 pt-5">
+          <img className="object-cover w-full h-28" src="/src/assets/dummy1.png" />
+          <img className="object-cover w-full h-28" src="/src/assets/dummy2.jpg" />
+          <img className="object-cover w-full h-28" src="/src/assets/dummy1.png" />
+          <img className="object-cover w-full h-28" src="/src/assets/dummy2.jpg" />
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default PhotoAlbumTab;
