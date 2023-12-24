c20c7583632dee63a4715e6b21a573aaea4052d2
hyun812 <sh980812@naver.com>
2023-12-25 01:12:53 +0900

    style #11 : 발자국 조회 UI 구현

--- src/pages/footprint/components/FootprintTab.tsx
+++ src/pages/footprint/components/FootprintTab.tsx
@@ -1,5 +1,35 @@
 function FootprintTab() {
-  return <div>발자국 조회</div>;
+  return (
+    <div className="w-full px-4">
+      {/* 날자별 반복 */}
+      <div className="pt-6">
+        <p className="font-bold">2023년 12월 14일</p>
+        {/* 발자국별 반복 */}
+        <div className="grid grid-cols-3 gap-5 pt-5">
+          <div className="flex flex-col items-center justify-center">
+            <img src="/src/assets/green-paw-icon.png" />
+            <p>차차</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img src="/src/assets/green-paw-icon.png" />
+            <p>원쥬</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img src="/src/assets/green-paw-icon.png" />
+            <p>승현</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img src="/src/assets/green-paw-icon.png" />
+            <p>차</p>
+          </div>
+          <div className="flex flex-col items-center justify-center">
+            <img src="/src/assets/green-paw-icon.png" />
+            <p>원</p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default FootprintTab;
