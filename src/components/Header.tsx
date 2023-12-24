8bd42c654ba6f2f0b2af4becb0bb486d324ca304
Lee Wonju <dldnjswn4265@gmail.com>
2023-12-24 13:37:05 +0900

    feat #9 : 헤더 높이 h-10 지정

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -5,7 +5,7 @@ const Header = (props: HeaderProp) => {
   const { left_func, right_func } = props.func;
 
   return (
-    <div className="w-full flex justify-between px-5">
+    <div className="w-full flex justify-between items-center px-5 h-10 bg-purple-300">
       <div onClick={left_func}>{left}</div>
       <div onClick={right_func}>{right}</div>
     </div>
