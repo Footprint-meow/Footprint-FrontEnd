6221103cf84e43008ac52e0dc164401729fe60f2
이승현 <sh980812@naver.com>
2023-12-29 20:54:10 +0900

    refactor #21 : 탭 클릭 애니메이션 구현

--- src/pages/footprint/components/Footprint.tsx
+++ src/pages/footprint/components/Footprint.tsx
@@ -2,46 +2,55 @@ import { useEffect, useState } from 'react';
 import FootprintTab from './FootprintTab';
 import GuestBookInfo from './GuestBookInfo';
 import PhotoAlbumTab from './PhotoAlbumTab';
+import '../animations.css';
 
-type tab = 0 | 1;
+type TabIndex = 0 | 1;
+type TabInfo = { index: TabIndex; tabTitle: string; content: React.ReactNode };
 
 const Footprint = () => {
-  const [tab, setTab] = useState<tab>(0);
+  const [tab, setTab] = useState<TabIndex>(0);
 
-  const changeTab = (index: number) => {
+  const tabArr: TabInfo[] = [
+    {
+      index: 0,
+      tabTitle: '발자국 조회',
+      content: <FootprintTab />,
+    },
+    {
+      index: 1,
+      tabTitle: '사진첩 보기',
+      content: <PhotoAlbumTab />,
+    },
+  ];
+
+  const changeTab = (index: TabIndex) => {
     setTab(index);
   };
   return (
     <div className="absolute top-0 w-full pt-16 z-15">
       <GuestBookInfo />
-      <div className="flex pt-10 text-center text-primary-1">
-        <div
-          onClick={() => {
-            changeTab(0);
-          }}
-          className={
-            tab === 0
-              ? 'border-b-[3px] font-bold flex-1 border-primary-1'
-              : 'border-b-[1px] text-primary-2 font-base flex-1 border-primary-2'
-          }
-        >
-          <p>발자국 조회</p>
-        </div>
-        <div
-          onClick={() => {
-            changeTab(1);
-          }}
-          className={
-            tab === 1
-              ? 'border-b-[3px] font-bold flex-1 border-primary-1'
-              : 'border-b-[1px] font-base flex-1 text-primary-2 border-primary-2'
-          }
-        >
-          <p>사진첩 보기</p>
+      <div className="relative flex w-full pt-10 pb-2 text-center text-primary-1">
+        {tabArr.map((item) => {
+          return (
+            <div
+              key={item.index}
+              onClick={() => {
+                changeTab(item.index);
+              }}
+              className={item.index === tab ? ' font-bold flex-1 ' : 'text-primary-2 flex-1 '}
+            >
+              <p>{item.tabTitle}</p>
+            </div>
+          );
+        })}
+        <div className="absolute bottom-0 left-0 w-full h-[1px] bg-primary-2">
+          <div
+            className={`left-0 slide relative bottom-[2px] z-10 w-1/2 h-[3px] bg-primary-1 ${'left-' + tab + '/2'}`}
+          ></div>
         </div>
       </div>
       {/* 탭에 따른 컴포넌트 보여주기 */}
-      {tab === 0 ? <FootprintTab /> : <PhotoAlbumTab />}
+      {tabArr[tab].content}
     </div>
   );
 };
