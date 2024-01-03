3e2e864fe03837f1c67057572fb333c5e41d3415
Lee Wonju <dldnjswn4265@gmail.com>
2024-01-03 19:22:44 +0900

    feat #30 : qr 스캔 후 버튼 표시

--- src/pages/guestbook/components/QRScanner.tsx
+++ src/pages/guestbook/components/QRScanner.tsx
@@ -1,25 +1,34 @@
-import React, { useRef, useEffect } from "react";
+import React, { useRef, useEffect, useState } from "react";
 import QrScanner from "qr-scanner";
 
 const QRScanner: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  // url 타입 정의
+  const [moveUrl, setMoveUrl] = useState<string | null>(null);
 
   useEffect(() => {
+    const handleQrCodeScan = (result: string) => {
+      console.log("QR Code Result:", result);
+      if (result && moveUrl === null) {
+        console.log("result: ", result);
+        setMoveUrl(result);
+        console.log("moveUrl: ", moveUrl);
+      }
+    };
+
     if (videoRef.current) {
-      const scanner = new QrScanner(videoRef.current, (result: string) => {
-        console.log("QR Code Result:", result);
-        // 결과를 원하는 방식으로 처리할 수 있습니다.
-      });
+      const scanner = new QrScanner(videoRef.current, handleQrCodeScan);
 
-      // 스캐너를 시작합니다.
       scanner.start();
+      return () => {
+        scanner.destroy();
+      };
     }
-
-    // 컴포넌트가 언마운트될 때 스캐너를 정지합니다.
-    // return () => {
-    //   scanner.stop();
-    // };
   }, []);
+
+  useEffect(() => {
+    console.log("moveUrl updated:", moveUrl);
+  }, [moveUrl]);
 
   return (
     <div className="h-full overflow-hidden relative flex items-center justify-center">
@@ -30,6 +39,19 @@ const QRScanner: React.FC = () => {
           {/* @TODO: 하단div bg-transparent 적용안됨 현상 해결하기 */}
           <div className="text-white border-2 border-white h-2/5 w-8/12 mt-4 bg-transparent"></div>
         </div>
+        {moveUrl && (
+          <button
+            onClick={() => window.open(moveUrl as string)}
+            className="bg-white shadow-lg self-end h-20 w-9/12 animate-jump-in rounded-[3rem] mb-10 text-black flex flex-col items-center justify-center font-bold text-2xl"
+          >
+            방명록 이동하기
+            <p className="text-sm font-medium">🐾 나의 발자국을 남겨보세요!</p>
+          </button>
+        )}
+        {/* 이승현이 이쁘다한 UI */}
+        {/* <div className="bg-white animate-jump-in shadow-lg self-end h-10 w-1/3 rounded-3xl mb-10 text-black flex flex-col items-center justify-center font-semibold text-lg">
+          이동하기
+        </div> */}
       </div>
     </div>
   );
