24a69b9319ce086c6ced89f3398102e12e37a91b
Lee Wonju <dldnjswn4265@gmail.com>
2024-01-03 20:22:49 +0900

    feat #29 : GuestBookInfo 컴포넌트 적용

--- src/pages/guestbook/GuestBookPage.tsx
+++ src/pages/guestbook/GuestBookPage.tsx
@@ -3,9 +3,19 @@ import { IMenu, IMenuFunc } from "../../types/header";
 import { icons } from "../../constants/header-icons";
 import Header from "../../components/Header";
 import { Link } from "react-router-dom";
+import { IGuestBookInfo } from "../footprint/interfaces";
+import GuestBookInfo from "../../components/GuestBookInfo";
 
 function GuestBookPage() {
   const navigate = useNavigate();
+
+  const guestBookInfo: IGuestBookInfo = {
+    photo: '/src/assets/dummy2.jpg',
+    name: '원쥬의 홈',
+    address: '경상북도 구미시 인의동',
+    count: 24,
+  };
+
   const menu: IMenu = {
     left: icons.BACK,
     center: "방명록",
@@ -23,34 +33,7 @@ function GuestBookPage() {
       <Header menu={menu} func={func} />
       <div className="h-[calc(100vh-11rem)] pt-2 w-full absolute bottom-0 bg-white flex flex-col items-center z-10 rounded-t-[3.2rem]">
         {/* @TODO: 방명록 타이틀 컴포넌트로 수정 */}
-        <div className="flex items-center w-[calc(100%-1rem)] px-6 pl-3 rounded-full bg-sub-2">
-          <img
-            src="/src/assets/dummy2.jpg"
-            className="object-cover w-20 h-20 rounded-full"
-          />
-          <div className="bg-white rounded-full "></div>
-          <div className="flex flex-col gap-1 p-2 basis-4/5">
-            <div className="flex items-center gap-3">
-              <p className="font-bold ">원쥬의 홈</p>
-              <img
-                src="/src/assets/green-paw-icon.png"
-                alt="green-paw-icon"
-                className="w-5 h-4"
-              />
-              <p className="font-bold text-primary-1">24</p>
-            </div>
-            <div className="text-sm">경상 북도 구미시 인의동</div>
-            <div className="flex justify-between gap-2 text-center text-white">
-              <div className="flex-1 p-1 text-sm rounded-md bg-primary-1">
-                방명록 편집
-              </div>
-              <div className="flex-1 p-1 text-sm rounded-md bg-primary-1">
-                방명록 공유
-              </div>
-            </div>
-          </div>
-        </div>
-
+        <GuestBookInfo info={guestBookInfo} hidden={"hidden"} />
         <div className="w-full px-7">
           <div className="border-slate-800 rounded-lg h-full">
             어서와 우리집은 처음이지
