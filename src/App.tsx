a9457c13e8b73e88c276b511a10ea95955ac151d
hyun812 <sh980812@naver.com>
2024-01-05 14:07:31 +0900

    feat #37 : router 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,66 +1,83 @@
-import { RouterProvider, createBrowserRouter } from 'react-router-dom';
-import GuestBookPage from './pages/guestbook/GuestBookPage';
-import FootprintPage from './pages/footprint/FootprintPage';
-import LoginPage from './pages/login/LoginPage';
-import SignUpPage from './pages/signup/SignUpPage';
-import HomePage from './pages/home/HomePage';
-import Profile from './pages/guestbook/AddGuestBook/components/Profile';
-import Photo from './pages/guestbook/AddGuestBook/components/Photo';
-import Description from './pages/guestbook/AddGuestBook/components/Description';
-import AddGuestBook from './pages/guestbook/AddGuestBook/AddGuestBook';
-import AddStep from './pages/footprint/components/AddStep';
-import AddPhoto from './pages/footprint/components/AddPhoto';
-import Footprint from './pages/footprint/components/Footprint';
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import GuestBookPage from "./pages/guestbook/GuestBookPage";
+import FootprintPage from "./pages/footprint/FootprintPage";
+import LoginPage from "./pages/login/LoginPage";
+import SignUpPage from "./pages/signup/SignUpPage";
+import HomePage from "./pages/home/HomePage";
+import Profile from "./pages/guestbook/AddGuestBook/components/Profile";
+import Photo from "./pages/guestbook/AddGuestBook/components/Photo";
+import Description from "./pages/guestbook/AddGuestBook/components/Description";
+import AddGuestBook from "./pages/guestbook/AddGuestBook/AddGuestBook";
+import AddStep from "./pages/footprint/AddStep/AddStep";
+import AddPhoto from "./pages/footprint/components/AddPhoto";
+import Footprint from "./pages/footprint/components/Footprint";
+import Writer from "./pages/footprint/AddStep/components/Writer";
+import Impression from "./pages/footprint/AddStep/components/Impression";
+import Password from "./pages/footprint/AddStep/components/Password";
 
 const router = createBrowserRouter([
   {
-    path: '/',
+    path: "/",
     element: <HomePage />,
   },
   {
-    path: '/guestbook',
+    path: "/guestbook",
     element: <GuestBookPage />,
   },
   {
-    path: '/footprint',
+    path: "/footprint",
     element: <FootprintPage />,
     children: [
       {
-        path: '',
+        path: "",
         element: <Footprint />,
       },
       {
-        path: 'add-step',
-        element: <AddStep />,
+        path: "add-photo",
+        element: <AddPhoto />,
       },
       {
-        path: 'add-photo',
-        element: <AddPhoto />,
+        path: "add-step",
+        element: <AddStep />,
+        children: [
+          {
+            path: "",
+            element: <Writer />,
+          },
+          {
+            path: "impression",
+            element: <Impression />,
+          },
+          {
+            path: "pwd",
+            element: <Password />,
+          },
+        ],
       },
     ],
   },
   {
-    path: '/login',
+    path: "/login",
     element: <LoginPage />,
   },
   {
-    path: '/signup',
+    path: "/signup",
     element: <SignUpPage />,
   },
   {
-    path: '/add-guestbook',
+    path: "/add-guestbook",
     element: <AddGuestBook />,
     children: [
       {
-        path: '',
+        path: "",
         element: <Profile />,
       },
       {
-        path: 'photo',
+        path: "photo",
         element: <Photo />,
       },
       {
-        path: 'desc',
+        path: "desc",
         element: <Description />,
       },
     ],
--- src/pages/footprint/FootprintPage.tsx
+++ src/pages/footprint/FootprintPage.tsx
@@ -1,24 +1,24 @@
-import { icons } from '../../constants/header-icons';
-import { IMenuFunc, IMenu } from '../../types/header';
-import Header from '../../components/Header';
-import { Outlet } from 'react-router-dom';
+import { icons } from "../../constants/header-icons";
+import { IMenuFunc, IMenu } from "../../types/header";
+import Header from "../../components/Header";
+import { Outlet, useNavigate } from "react-router-dom";
 
 function FootprintPage() {
-  // const navigate = useNavigate();
+  const navigate = useNavigate();
 
   const menu: IMenu = {
     left: icons.BACK,
-    center: '방명록',
+    center: "방명록",
     right: null,
   };
 
   const func: IMenuFunc = {
-    left_func: null,
+    left_func: () => navigate(-1),
     right_func: null,
   };
 
   return (
-    <div className="relative w-full h-screen">
+    <div className="w-full h-screen">
       <Header menu={menu} func={func} />
       <Outlet />
     </div>
